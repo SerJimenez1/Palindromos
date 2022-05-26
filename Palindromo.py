0d1a756055a02639d86ef0acfbd2f1e2d938722e
SerJimenez1 <106128413+SerJimenez1@users.noreply.github.com>
2022-05-25 20:22:23 -0500

    Update Palindromo.py

--- Palindromo.py
+++ Palindromo.py
@@ -1,11 +1,43 @@
 def palindromo(oracion):
     oracion = oracion.replace(" ","")
     if str(oracion)==str(oracion)[::-1]:
-      return True
+      print("La oración sí es un palíndromo")
     else:
-      return False  
-print(palindromo(input("Escriba una oracion  ")))
-        
+      print("La oración no es un palíndromo")
+
+      
+def ingresar_oracion():  
+    while True:
+        error = False
+        oracion = input("Escriba una oración: ")
+        for letra in oracion:
+            if letra.isdigit() == True:
+                print("Ingrese una oración que no contenga números")
+                error = True
+                break
+        if error == False:
+            break
+
+    return oracion
+
+def menu():
+    while True:
+        oracion = ingresar_oracion()
+        palindromo(oracion)
+        while True:
+            opc = input("¿Desea ingresar otra oración? si/no: ")
+            if opc != "si" and opc != "no":
+                print("Ingrese alguna de las opciones")
+                continue
+            elif opc == "si":
+                break
+            else:
+                print("¡Hasta luego!")
+                quit()
+
+menu()
+
+
 
 
 
